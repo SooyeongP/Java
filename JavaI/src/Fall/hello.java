7f5018752a3d6f21a50c5af6ec6a8f706c5b5cc7
basket-100 <basket-100@localhost>
2021-09-14 20:14:56 -0400

    Homework

--- JavaI/src/Fall/hello.java
+++ JavaI/src/Fall/hello.java
@@ -3,8 +3,12 @@ package Fall;
 public class hello {
 
 	public static void main(String[] args) {
-		
-
+		System.out.println("What is my favorite icecream flavor?");
+		System.out.println("Chocolate");
+		System.out.println("Do I have siblings?");
+		System.out.println("One Brother and one sister");
+		System.out.println("Where am I from?");
+		System.out.println("Korea");
 	}
 
 }
