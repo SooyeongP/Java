75b0f64e1b942094bd1d10bcc4e4b53f66630750
basket-100 <basket-100@10.1.67.104>
2021-09-22 12:52:01 -0400

    hw2

---
+++ JavaI/src/Fall/sep21_2.java
@@ -0,0 +1,10 @@
+package Fall;
+
+public class sep21_2 {
+
+	public static void main(String[] args) {
+		int x=98;
+System.out.println(x %10);
+		
+	}
+}
---
+++ JavaI/src/Fall/sep21_3.java
@@ -0,0 +1,10 @@
+package Fall;
+
+public class sep21_3 {
+	public static void main(String[] args) {
+		int b=4;
+		int a =2;
+		System.out.println((double)(a+b)/2);
+	}
+
+}
